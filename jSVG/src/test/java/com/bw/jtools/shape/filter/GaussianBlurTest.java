5ee440c36425a2157cb294e3e17319581651be20
Bernd Wengenroth <Bernd.Wengenroth@gmx.de>
2022-03-19 15:18:23 +0100

    Fixed junit tests

--- jSVG/src/test/java/com/bw/jtools/shape/filter/GaussianBlurTest.java
+++ jSVG/src/test/java/com/bw/jtools/shape/filter/GaussianBlurTest.java
@@ -27,8 +27,6 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GaussianBlurTest
 {
 	@Test
