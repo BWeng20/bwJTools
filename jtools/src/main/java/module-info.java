449e97dd1700d5c6190e52fbfc3bf8490cb7e4d6
Bernd Wengenroth <Bernd.Wengenroth@gmx.de>
2022-04-01 14:19:17 +0200

    Added explizit handlikng of unnamed modules to (hopefully finaly) solve module issues.

--- jtools/src/main/java/module-info.java
+++ jtools/src/main/java/module-info.java
@@ -1,18 +1,19 @@
 module com.bw.jtools {
-	// static: compile time but optional during runtime.
-	// (only needed if the related functionality is used).
-	requires static jdk.management;
-	requires static java.sql; // Needed by commons.csv
-	// transitive: using modules are also allowed to read.
-	requires static transitive java.json;
-	requires static transitive java.desktop;
-	requires transitive com.bw.jSVG;
 
-	requires static org.netbeans.swing.outline.RELEASE121;
-	requires static org.apache.logging.log4j.core;
-	requires static org.apache.logging.log4j;
+    // static: compile time but optional during runtime.
+    // (only needed if the related functionality is used).
+    // transitive: using modules are also allowed to read.
+    
+    requires static jdk.management;
+    requires java.json;
+    requires transitive java.desktop;
+    requires transitive com.bw.jSVG;
 
-	requires java.prefs;
-	requires commons.csv;
-	exports com.bw.jtools;
+    requires transitive org.netbeans.swing.outline;
+    requires transitive commons.csv;
+    requires static org.apache.logging.log4j.core;
+    requires static org.apache.logging.log4j;
+
+    requires java.prefs;
+    exports com.bw.jtools;
 }
